713e7be46ba045cb809d353b93397fdc069a83f4
Ermi24et <lilermi24@gmail.com>
2024-04-16 04:10:18 +0300

    added an auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """a module that manage the API authentication"""
 from flask import request
+from fnmatch import fnmatch
 from typing import List, TypeVar
 
 
@@ -8,7 +9,11 @@ class Auth:
     """a class template for all authentication system"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """a public method that returns False - path and exluded_path"""
-        return False
+        if not excluded_paths or not path:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        return not [n for n in excluded_paths if fnmatch(path, n)]
 
     def authorization_header(self, request=None) -> str:
         """a method that returns None temporarily"""
