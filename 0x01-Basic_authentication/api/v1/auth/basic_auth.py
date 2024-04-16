7b618c0b41024e5f615af3372944d31c17f9a9c2
Ermi24et <lilermi24@gmail.com>
2024-04-16 16:32:37 +0300

    basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """a module that contains subclass of Auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,4 +14,16 @@ class BasicAuth(Auth):
             if authorization_header[0: 6] == 'Basic ':
                 return authorization_header[len("Basic "):]
             return None
+        return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """a method that returns the decoded value of a Base64 string"""
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    bytes_seq = base64.b64decode(base64_authorization_header)
+                    return bytes_seq.decode('utf-8')
+                except Exception:
+                    return None
         return None
