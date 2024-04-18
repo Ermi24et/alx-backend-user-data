2b762055cbafed3b6128ab9c6b841ba1a555f2d9
Ermi24et <lilermi24@gmail.com>
2024-04-18 14:36:54 +0300

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module that contains SessionAuth class that inherits from Auth"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class used to create a session authentication mechanism"""
+    pass
