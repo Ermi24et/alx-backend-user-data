79022edacf5c556910727bd953246a86da016294
Ermi24et <lilermi24@gmail.com>
2024-04-20 17:16:52 +0300

    logging out

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,3 +31,13 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
+
+    def destroy_session(self, request=None):
+        """deletes the user session"""
+        if not request:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id or not self.user_id_for_session_id(session_id):
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
