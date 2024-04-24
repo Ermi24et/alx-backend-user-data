d23b3fe0233b3b2cd18313b380dc4da50f4d0be1
Ermi24et <lilermi24@gmail.com>
2024-04-24 22:53:45 +0300

    credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,11 @@ class Auth:
         else:
             raise ValueError(f"User {email} already exists")
         return user
+
+    def valid_login(self, email: str, password: str):
+        """returns true if the user exists and false if not"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
